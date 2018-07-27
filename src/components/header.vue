17f9c601b1336d2380acb2735593e4505dd73039
shixiang <shixiangcool@163.com>
2018-07-27 23:56:25 +0800

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <div class="msg">歌曲管理</div>
+    <div class="logo"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'header'
+}
+</script>
+
+<style scoped>
+  .header{height: 100px; background: green; display: flex; flex-direction: row;}
+</style>
